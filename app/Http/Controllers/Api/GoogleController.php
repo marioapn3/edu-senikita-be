a7e954194c3400114a025af8e506c37cd7bbaee0
marioapn3 <mario.aprilnino27@gmail.com>
2025-05-04 22:21:27 +0700

    google client

--- app/Http/Controllers/Api/GoogleController.php
+++ app/Http/Controllers/Api/GoogleController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Models\User;
 use Exception;
+use Google_Client;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Str;
